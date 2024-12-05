c58041b00202d8843b473161921866d4d6d260d1
Nicolás Comerci <ncomerci@outlook.com>
2024-12-05 03:12:11 -0300

    Cleanup for initial alpha release

--- contrib/embeddings.cpp/models/run_conversions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-model=$1
-
-python3 convert-to-ggml.py ${model} 0
-python3 convert-to-ggml.py ${model} 1
-../build/bin/quantize ${model}/ggml-model-f16.bin ${model}/ggml-model-q4_0.bin 2
-../build/bin/quantize ${model}/ggml-model-f16.bin ${model}/ggml-model-q4_1.bin 3
